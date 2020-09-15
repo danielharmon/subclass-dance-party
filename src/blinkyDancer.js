94852c963c2a5e2ab6220ccf3c5983d3ef58c713
Danny Harmon <dannyaharmon@gmail.com>
2020-09-14 18:25:42 -0700

    Debugging

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,15 +1,16 @@
 var MakeBlinkyDancer = function(top, left, timeBetweenSteps) {
   //create object
   MakeDancer.call(this, top, left, timeBetweenSteps);
-  this.oldStep = this.step;
+  this.oldStep = MakeDancer.prototype.step;
+  console.log(this.oldStep);
   //return object
 };
 
-MakeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
+MakeBlinkyDancer.prototype = Object.create(MakeDancer.prototype);
 
 MakeBlinkyDancer.prototype.constructor = MakeBlinkyDancer;
 
-// MakeBlinkyDancer.prototype.step = function() {
-//   this.oldStep();
-//   this.$node.toggle();
-// };
+MakeBlinkyDancer.prototype.step = function() {
+  this.oldStep();
+  this.$node.toggle();
+};
